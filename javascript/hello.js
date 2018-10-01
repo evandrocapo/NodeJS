fc91b615e6ed3753a258c71d446b682375b11718
EVANDRO DOUGLAS CAPOVILLA JUNIOR <evandro.dcj@puccampinas.edu.br>
2018-10-01 11:08:55 -0300

    hello atualizado com mySql

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,11 +1,25 @@
 var http = require('http');
-var util = require('./util.js')
+var util = require('./util.js');
+var mySQL = require('mysql');
 
 http.createServer(function(request,response){
     response.writeHead(200, {'Content-type': 'text/plain'});
-    console.log('Teste')
     response.end('Hello World!' + util.dateTime());
-
-
+    console.log('Teste');
 }
 ).listen(3600);
+
+var conMySQL = mySQL.createConnection({
+    host: 'esparta',
+    port: 3306,
+    user: 'patriciadados',
+    password: 'dados',
+    database: 'patricia'
+});
+
+var retornaDados = function(error,results){
+    return result.stringfy(result);
+};
+
+var query = 'SELECT * FROM pessoa';
+conMySQL.query(query, retornaDados);
