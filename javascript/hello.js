24711c4cd81e76b8c5aa03af680f40d12599bd60
EVANDRO DOUGLAS CAPOVILLA JUNIOR <evandro.dcj@puccampinas.edu.br>
2018-10-01 10:37:36 -0300

    Hello Atulizado

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var util = require('./util.js')
 
 http.createServer(function(request,response){
     response.writeHead(200, {'Content-type': 'text/plain'});
-    response.end('Hello World!');
+    console.log('Teste')
+    response.end('Hello World!' + util.dateTime());
 
 
 }
