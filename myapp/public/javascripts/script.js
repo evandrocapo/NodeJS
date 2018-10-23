0784d0373b54f2af16819d1276c881286b6aee9c
EVANDRO DOUGLAS CAPOVILLA JUNIOR <evandro.dcj@puccampinas.edu.br>
2018-10-23 16:37:13 -0300

    Atualização automatica da pagina.

--- myapp/public/javascripts/script.js
+++ myapp/public/javascripts/script.js
@@ -1,12 +1,14 @@
 function exibeClientes(clientes) {
     for (var i = 0; i < clientes.length; i++) {
         var cliente = clientes[i];
-        var dadosCliente = 'ID: ' + cliente.id +
+        var dadosCliente = '<div id="' + cliente.id + '">' +
+            'ID: ' + cliente.id +
             '<br>Nome: ' + cliente.nome +
             '<br>Endereço: ' + cliente.endereco +
             '<br>Telefone: ' + cliente.telefone +
             '<br>Email: ' + cliente.email +
-            '<br><a href="#" onClick="deletaCliente(' + cliente.id + ')">EXCLUIR</a>';
+            '<br><a href="#" onClick="deletaCliente(' + cliente.id + ')">EXCLUIR</a>' +
+            '<div>';
         document.getElementById('result').innerHTML += dadosCliente + '<br><br>';
     }
 }
@@ -23,8 +25,11 @@ function deletaCliente(id) {
                 success: function (dados) {
                     if (dados.status === 'ERRO')
                         alert('Erro: ' + dados.data);
-                    else
+                    else{
                        alert(dados.data);
+                       var divResult = document.getElementById('result');
+                       divResult.removeChild(document.getElementById(id));
+                    }
                 }
             });
         });
