37d3990df7ce2312e30f41781e6b8744179386e5
EVANDRO DOUGLAS CAPOVILLA JUNIOR <evandro.dcj@puccampinas.edu.br>
2018-10-23 16:17:38 -0300

    Função deleta

--- myapp/public/javascripts/script.js
+++ myapp/public/javascripts/script.js
@@ -5,7 +5,27 @@ function exibeClientes(clientes) {
             '<br>Nome: ' + cliente.nome +
             '<br>Endereço: ' + cliente.endereco +
             '<br>Telefone: ' + cliente.telefone +
-            '<br>Email: ' + cliente.email;
+            '<br>Email: ' + cliente.email +
+            '<br><a href="#" onClick="deletaCliente(' + cliente.id + ')">EXCLUIR</a>';
         document.getElementById('result').innerHTML += dadosCliente + '<br><br>';
     }
+}
+
+function deletaCliente(id) {
+    $(document).ready(function () {
+            $.ajax({
+                url: '/cliente/deleta?id=' + id,
+                dataType: 'json',
+                type: 'post',
+                error: function (dados) {
+                    alert('Erro: ' + dados.data);
+                },
+                success: function (dados) {
+                    if (dados.status === 'ERRO')
+                        alert('Erro: ' + dados.data);
+                    else
+                       alert(dados.data);
+                }
+            });
+        });
 }
--- myapp/routes/cliente.js
+++ myapp/routes/cliente.js
@@ -15,4 +15,18 @@ router.get('/lista', function (req, res, next) {
     });
 });
 
+router.post('/deleta', function (req, res, next) {
+    var id = req.query.id;
+    req.getConnection(function (err, connection) {
+        var query = connection.query("DELETE FROM cliente WHERE id = " + id, function (err, rows) {
+            if (err)
+                res.json({ status: 'ERROR', data: + err });
+            else
+                res.json({ status: 'OK', data: 'Excluido com sucesso' });
+        });
+        if (err)
+            res.json({ status: 'ERROR', data: + err });
+    });
+});
+
 module.exports = router;
